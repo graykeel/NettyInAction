99f38599c0a34beb72dda0bcbad604e323aeab08
moreus <weicheng.mao@gmail.com>
2014-09-01 14:10:03 +0800

    format.
    Change-Id: I848ebf625426c874df99058d5b4c9c71704ac2a7
    Signed-off-by: moreus <weicheng.mao@gmail.com>

--- src/com/wii/netty/frame/correct/TimeServerHandler.java
+++ src/com/wii/netty/frame/correct/TimeServerHandler.java
@@ -27,23 +27,23 @@ import io.netty.channel.ChannelHandlerContext;
  */
 public class TimeServerHandler extends ChannelHandlerAdapter {
 
-    private int counter;
+	private int counter;
 
-    @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg)
-	    throws Exception {
-	String body = (String) msg;
-	System.out.println("The time server receive order : " + body
-		+ " ; the counter is : " + ++counter);
-	String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new java.util.Date(
-		System.currentTimeMillis()).toString() : "BAD ORDER";
-	currentTime = currentTime + System.getProperty("line.separator");
-	ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
-	ctx.writeAndFlush(resp);
-    }
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg)
+			throws Exception {
+		String body = (String) msg;
+		System.out.println("The time server receive order : " + body
+				+ " ; the counter is : " + ++counter);
+		String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new java.util.Date(
+				System.currentTimeMillis()).toString() : "BAD ORDER";
+		currentTime = currentTime + System.getProperty("line.separator");
+		ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
+		ctx.writeAndFlush(resp);
+	}
 
-    @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-	ctx.close();
-    }
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+		ctx.close();
+	}
 }
