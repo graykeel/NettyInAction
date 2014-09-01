f94e895cc6308c527b7ea6bfb066a08d8df40a86
moreus <weicheng.mao@gmail.com>
2014-09-01 13:47:07 +0800

    format, read code.
    Change-Id: I6250268500b934076f3ee6dfbb3791d7dd0c7642
    Signed-off-by: moreus <weicheng.mao@gmail.com>

--- src/com/wii/netty/aio/AcceptCompletionHandler.java
+++ src/com/wii/netty/aio/AcceptCompletionHandler.java
@@ -25,20 +25,20 @@ import java.nio.channels.CompletionHandler;
  * @version 1.0
  */
 public class AcceptCompletionHandler implements
-	CompletionHandler<AsynchronousSocketChannel, AsyncTimeServerHandler> {
+		CompletionHandler<AsynchronousSocketChannel, AsyncTimeServerHandler> {
 
-    @Override
-    public void completed(AsynchronousSocketChannel result,
-	    AsyncTimeServerHandler attachment) {
-	attachment.asynchronousServerSocketChannel.accept(attachment, this);
-	ByteBuffer buffer = ByteBuffer.allocate(1024);
-	result.read(buffer, buffer, new ReadCompletionHandler(result));
-    }
+	@Override
+	public void completed(AsynchronousSocketChannel result,
+			AsyncTimeServerHandler attachment) {
+		attachment.asynchronousServerSocketChannel.accept(attachment, this);
+		ByteBuffer buffer = ByteBuffer.allocate(1024);
+		result.read(buffer, buffer, new ReadCompletionHandler(result));
+	}
 
-    @Override
-    public void failed(Throwable exc, AsyncTimeServerHandler attachment) {
-	exc.printStackTrace();
-	attachment.latch.countDown();
-    }
+	@Override
+	public void failed(Throwable exc, AsyncTimeServerHandler attachment) {
+		exc.printStackTrace();
+		attachment.latch.countDown();
+	}
 
 }
