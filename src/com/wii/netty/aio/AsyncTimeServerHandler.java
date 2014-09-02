fe749b98013a3b33c1eada64e849ce726194d575
moreus <weicheng.mao@gmail.com>
2014-09-02 16:02:18 +0800

    read code.
    Change-Id: I49bddadaad7706a69671cc29037e7621357876f7
    Signed-off-by: moreus <weicheng.mao@gmail.com>

--- src/com/wii/netty/aio/AsyncTimeServerHandler.java
+++ src/com/wii/netty/aio/AsyncTimeServerHandler.java
@@ -27,43 +27,43 @@ import java.util.concurrent.CountDownLatch;
  */
 public class AsyncTimeServerHandler implements Runnable {
 
-    private int port;
+	private int port;
 
-    CountDownLatch latch;
-    AsynchronousServerSocketChannel asynchronousServerSocketChannel;
+	CountDownLatch latch;
+	AsynchronousServerSocketChannel asynchronousServerSocketChannel;
 
-    public AsyncTimeServerHandler(int port) {
-	this.port = port;
-	try {
-	    asynchronousServerSocketChannel = AsynchronousServerSocketChannel
-		    .open();
-	    asynchronousServerSocketChannel.bind(new InetSocketAddress(port));
-	    System.out.println("The time server is start in port : " + port);
-	} catch (IOException e) {
-	    e.printStackTrace();
+	public AsyncTimeServerHandler(int port) {
+		this.port = port;
+		try {
+			asynchronousServerSocketChannel = AsynchronousServerSocketChannel
+					.open();
+			asynchronousServerSocketChannel.bind(new InetSocketAddress(port));
+			System.out.println("The time server is start in port : " + port);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-    }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.lang.Runnable#run()
-     */
-    @Override
-    public void run() {
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Runnable#run()
+	 */
+	@Override
+	public void run() {
 
-	latch = new CountDownLatch(1);
-	doAccept();
-	try {
-	    latch.await();
-	} catch (InterruptedException e) {
-	    e.printStackTrace();
+		latch = new CountDownLatch(1);
+		doAccept();
+		try {
+			latch.await();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
-    }
 
-    public void doAccept() {
-	asynchronousServerSocketChannel.accept(this,
-		new AcceptCompletionHandler());
-    }
+	public void doAccept() {
+		asynchronousServerSocketChannel.accept(this,
+				new AcceptCompletionHandler());
+	}
 
 }
--- src/com/wii/netty/codec/pojo/SubscribeResp.java
+++ src/com/wii/netty/codec/pojo/SubscribeResp.java
@@ -24,71 +24,71 @@ import java.io.Serializable;
  */
 public class SubscribeResp implements Serializable {
 
-    /**
-     * 默认序列ID
-     */
-    private static final long serialVersionUID = 1L;
+	/**
+	 * 默认序列ID
+	 */
+	private static final long serialVersionUID = 1L;
 
-    private int subReqID;
+	private int subReqID;
 
-    private int respCode;
+	private int respCode;
 
-    private String desc;
+	private String desc;
 
-    /**
-     * @return the subReqID
-     */
-    public final int getSubReqID() {
-	return subReqID;
-    }
+	/**
+	 * @return the subReqID
+	 */
+	public final int getSubReqID() {
+		return subReqID;
+	}
 
-    /**
-     * @param subReqID
-     *            the subReqID to set
-     */
-    public final void setSubReqID(int subReqID) {
-	this.subReqID = subReqID;
-    }
+	/**
+	 * @param subReqID
+	 *            the subReqID to set
+	 */
+	public final void setSubReqID(int subReqID) {
+		this.subReqID = subReqID;
+	}
 
-    /**
-     * @return the respCode
-     */
-    public final int getRespCode() {
-	return respCode;
-    }
+	/**
+	 * @return the respCode
+	 */
+	public final int getRespCode() {
+		return respCode;
+	}
 
-    /**
-     * @param respCode
-     *            the respCode to set
-     */
-    public final void setRespCode(int respCode) {
-	this.respCode = respCode;
-    }
+	/**
+	 * @param respCode
+	 *            the respCode to set
+	 */
+	public final void setRespCode(int respCode) {
+		this.respCode = respCode;
+	}
 
-    /**
-     * @return the desc
-     */
-    public final String getDesc() {
-	return desc;
-    }
+	/**
+	 * @return the desc
+	 */
+	public final String getDesc() {
+		return desc;
+	}
 
-    /**
-     * @param desc
-     *            the desc to set
-     */
-    public final void setDesc(String desc) {
-	this.desc = desc;
-    }
+	/**
+	 * @param desc
+	 *            the desc to set
+	 */
+	public final void setDesc(String desc) {
+		this.desc = desc;
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.lang.Object#toString()
-     */
-    @Override
-    public String toString() {
-	return "SubscribeResp [subReqID=" + subReqID + ", respCode=" + respCode
-		+ ", desc=" + desc + "]";
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "SubscribeResp [subReqID=" + subReqID + ", respCode=" + respCode
+				+ ", desc=" + desc + "]";
+	}
 
 }
